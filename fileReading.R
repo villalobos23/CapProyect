a078e41eae4bd22db70a785fccaaa5c2f756c246
villalobos23 <villalobos23@gmail.com>
2016-08-28 23:11:22 -0400

    initial commit README pending

---
+++ fileReading.R
@@ -0,0 +1,47 @@
+getNumLines <- function(fileName){
+  testcon <- file(fileName,open="r")
+  readsizeof <- 20000
+  nooflines <- 0
+  ( while((linesread <- length(readLines(testcon,readsizeof))) > 0 ) 
+    nooflines <- nooflines+linesread )
+  close(testcon)
+  nooflines
+}
+
+getLongestLineLength <- function(fileName){
+  testcon <- file(fileName,open="r")
+  readsizeof <- 1
+  maxLength <- 0
+  maxString <- ""
+  while(length(lineread <- readLines(testcon,readsizeof)) > 0 ){
+    currentLength <- nchar(lineread)
+    if(currentLength > maxLength){
+      maxLength <- currentLength
+      maxString <- lineread
+    }
+  }
+  close(testcon)
+  print(maxString)
+  maxLength
+}
+
+countWordOccurrenceByLine <- function(fileName,word,printMatch=FALSE){
+  testcon <- file(fileName,open="r")
+  readsizeof <- 20000
+  wordFrequency <- 0
+  while(length(linesread <- readLines(testcon,readsizeof)) > 0 ){
+    matches <- grepl(word,linesread)
+    freqAmount <- length(which(matches))
+    wordFrequency <- wordFrequency + (1*freqAmount)
+    if(freqAmount > 0 && printMatch)
+    {
+      print(linesread[which(matches)])
+    }
+  }
+  close(testcon)
+  wordFrequency
+}
+
+extractFiles <- function(tarball){
+  unzip(tarball,overwrite = TRUE)
+}
